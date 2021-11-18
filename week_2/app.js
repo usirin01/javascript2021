f3b15d72b06d234f43d92c2546076356d51e66af
usirin01 <usirin2021@gmail.com>
2021-11-18 12:55:09 +0100

    JS Intro first commit

---
+++ week_2/app.js
@@ -0,0 +1,28 @@
+    //Java script kısmı Fonksiyon tanımladım id'si "kutu" olan tagin value'sunu alert ile ekrana basıyor.//
+
+    function EkranaBas()
+       {
+       alert(document.getElementById("kutu").value);
+       console.log(kutu);
+       }
+
+    //    //variable define//
+
+    var adSoyad = "Umit S.";
+    console.log(typeof adSoyad);
+     document.getElementById("name").innerHTML = adSoyad;
+ 
+    var tckn = 12345678910;
+    console.log(typeof tckn);
+    document.getElementById("id number").innerHTML = tckn;
+ 
+    var yas = 33;
+    console.log(typeof yas);
+    document.getElementById("age").innerHTML = yas;
+
+    var meslek
+    console.log(typeof meslek);
+    document.getElementById("job").innerHTML = meslek;
+
+
+ 
