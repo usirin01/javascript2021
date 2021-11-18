4f42028646c6fcf58804fac0b2939e998f951a5c
Umit S <89708166+usirin01@users.noreply.github.com>
2021-11-18 14:03:17 +0100

    JS Intro page was styled

---
+++ week_2/app.js
@@ -0,0 +1,29 @@
+    //Java script kısmı Fonksiyon tanımladım id'si "kutu" olan tagin value'sunu alert ile ekrana basıyor.//
+
+    function PushScreen()
+       {
+       alert(document.getElementById("box").value);
+       console.log(box.value);
+       document.write(box.value);
+       }
+
+    //variable define//
+
+    var adSoyad = "Umit S.";
+    console.log(typeof adSoyad);
+    document.getElementById("name").innerHTML = adSoyad;
+ 
+    var tckn = 12345678910;
+    console.log(typeof true);
+    document.getElementById("id number").innerHTML = tckn;
+ 
+    var yas = 33;
+    console.log(typeof yas);
+    document.getElementById("age").innerHTML = yas;
+
+    var meslek =
+    console.log(typeof meslek);
+    document.getElementById("job").innerHTML = meslek;
+
+
+ 
