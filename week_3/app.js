252ba9864113de97b8351edbb8b237c725ba800d
usirin01 <usirin2021@gmail.com>
2021-11-20 10:01:44 +0100

    age calculator

---
+++ week_3/app.js
@@ -0,0 +1,26 @@
+//Console kisminda mantiksal islem
+
+//yas 13'ten kücük mü? o zaman bu sahis cocuktur
+//Eger yas 13 ile 21 arasinda ise, bu sahis genctir.
+//eger yas 21 ile 35 arasinda ise bu sahis yetiskindir.
+//Eger yas 60 üstü ise bu sahis ihtiyardir.
+
+let age = 40;
+
+if(age<13){
+    console.log("COCUK");
+}    
+
+if(age >= 13 && age < 35){
+    console.log("GENC");
+}    
+
+
+if(age >= 35 && age < 60){
+    console.log("ORTA YASLI");
+} 
+
+
+if(age >= 60){
+    console.log("GENC");
+}
