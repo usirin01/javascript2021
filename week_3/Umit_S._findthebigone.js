ed54241b21fc763b2288c1716129998eddd1366c
Umit S <89708166+usirin01@users.noreply.github.com>
2021-11-22 22:22:41 +0100

    added the algorithm expressions

--- week_3/Umit_S._findthebigone.js
+++ week_3/Umit_S._findthebigone.js
@@ -1,4 +1,10 @@
 //This defined function can comparis two numbers// 
+//1)we define the variable of the first number
+//2)we define the variable of the second number
+//3)we call to entered of first number and if it's bigger than other number, we show this number on the screen. After that, the program is ending.
+//4)we call to entered of second number and if it's bigger than other number, we show this number on the screen. Than the program is ending. 
+//5)if two numbers value is equal, we want to show "two numbers are equal" on the screen. Than the program is ending.
+
 function PushButton()
 {
 //first and second input area's structure were created//
