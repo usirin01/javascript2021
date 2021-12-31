f52d369e0786b823e2d0ec0460bd21628dd7fb17
usirin01 <usirin2021@gmail.com>
2021-12-31 23:58:55 +0100

    week2 second task code started

---
+++ javascript_104/week_2/app_2.js
@@ -0,0 +1,25 @@
+266 / 5,000
+// First, the salary determination in the given array
+// Second, getting only numbers from the array
+// Adding to the newly created array
+// Calculating company support
+// Print the result and finish the program 
+
+// The given array for Fikret's notebook
+
+const incomeStatement = [15000, 2000, 1000, 300, 200,
+    "important electronic", 600, 79, 
+    "abo", 342, "printer",450, 32, 45, 
+    "fitness", 64, 10, 23, 12, 2, 56, 7];
+
+// determine the salary of Fikret
+
+const fikomaas = incomeStatement[0];
+
+
+
+
+
+
+
+
