07cc20a00e8986808e153b51be35778ee47033b9
usirin01 <usirin2021@gmail.com>
2022-01-02 21:35:14 +0100

    week2 second task code has already finished

--- javascript_104/week_2/app_2.js
+++ javascript_104/week_2/app_2.js
@@ -7,14 +7,41 @@
 
 // The given array for Fikret's notebook
 
-const incomeStatement = [15000, 2000, 1000, 300, 200,
+let incomeStatement = [15000, 2000, 1000, 300, 200,
     "important electronic", 600, 79, 
     "abo", 342, "printer",450, 32, 45, 
     "fitness", 64, 10, 23, 12, 2, 56, 7];
 
-// determine the salary of Fikret
+// calculting the salary of Fikret
 
-const fikomaas = incomeStatement[0];
+let fikretpayment = incomeStatement[0];
+
+function findNumber(pNumber){
+    return pNumber.filter(x => typeof x === "number");
+}
+
+const sayilar = findNumber(incomeStatement);
+
+//
+
+let total = 0;
+sayilar.map((element) => total += Number(element));
+
+//variable determining
+
+let mountlyexpense = total - fikretpayment;
+let lastmoney = fikretpayment - mountlyexpense;
+
+//calculating of company supporting
+
+let companysupport = mountlyexpense/5;
+
+//showing to the console
+
+console.log(fikretpayment,"fikret's mountly amount");
+console.log(mountlyexpense,"total mountly expense");
+console.log(companysupport, "support of company");
+console.log(lastmoney, "last money after every mounth");
 
 
 
