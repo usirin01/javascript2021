3741e1f9efb65963fe4db6d00e55c865a0c8b630
usirin01 <usirin2021@gmail.com>
2022-01-22 08:40:43 +0100

    new arragement

--- javascript_104/week_5/function.js
+++ javascript_104/week_5/function.js
@@ -4,29 +4,61 @@
  */
 
 function chargeBattery(){
-    if (batteryPercent < 8){
-        batteryPercent ++
+    if (batteryPercent < 4){
+        batteryPercent = batteryPercent + 1
     }
-
-    console.log(batteryPercent); 
-}
-
-function dechargeBattery(){
-    if (batteryPercent > 0){
-        batteryPercent --
-    }
-
-    console.log(batteryPercent);  
-}
-
-function changeBatteryColor(){
     if (batteryPercent == 0){
         document.getElementById("row-3").style.backgroundColor = "white";
         document.getElementById("row-2").style.backgroundColor = "white";
         document.getElementById("row-1").style.backgroundColor = "white";
     }
-        
+    else if (batteryPercent == 1){
+        document.getElementById("row-3").style.backgroundColor = "red";
+        document.getElementById("row-2").style.backgroundColor = "white";
+        document.getElementById("row-1").style.backgroundColor = "white";
+    }
+    else if (batteryPercent == 2){
+        document.getElementById("row-3").style.backgroundColor = "yellow";
+        document.getElementById("row-2").style.backgroundColor = "yelllow";
+        document.getElementById("row-1").style.backgroundColor = "white";
+    }
+    else if (batteryPercent == 3){
+        document.getElementById("row-3").style.backgroundColor = "green";
+        document.getElementById("row-2").style.backgroundColor = "green";
+        document.getElementById("row-1").style.backgroundColor = "green";
+    }
+    console.log(batteryPercent); 
+    return
+    
 }
-changeBatteryColor();
+
+function dechargeBattery(){
+    if (batteryPercent >= 0){
+        batteryPercent = batteryPercent - 1
+    }
+    if (batteryPercent == 0){
+        document.getElementById("row-3").style.backgroundColor = "white";
+        document.getElementById("row-2").style.backgroundColor = "white";
+        document.getElementById("row-1").style.backgroundColor = "white";
+    }
+    if (batteryPercent == 0){
+        document.getElementById("row-3").style.backgroundColor = "red";
+        document.getElementById("row-2").style.backgroundColor = "white";
+        document.getElementById("row-1").style.backgroundColor = "white";
+    }
+    else if (batteryPercent == 1){
+        document.getElementById("row-3").style.backgroundColor = "yellow";
+        document.getElementById("row-2").style.backgroundColor = "yelllow";
+        document.getElementById("row-1").style.backgroundColor = "white";
+    }
+    else if (batteryPercent == 2){
+        document.getElementById("row-3").style.backgroundColor = "green";
+        document.getElementById("row-2").style.backgroundColor = "green";
+        document.getElementById("row-1").style.backgroundColor = "green";
+    }
+    console.log(batteryPercent); 
+    return
+     
+}
     
     
