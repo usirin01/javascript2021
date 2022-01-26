660a03bd46f18e26c627f88a9c4213271b0c6503
usirin01 <usirin2021@gmail.com>
2022-01-26 22:40:05 +0100

    week_6 second task app.js  page was created

---
+++ javascript_104/week_6/app.js
@@ -0,0 +1,17 @@
+/**
+ * Uygulama ilk baslatildiginda localStorage da
+ * initial value olusturulur
+ */
+ if (getFromLocalStorage("personList") === null) {
+    setToLocalStorage("personList", personList);
+  }
+  
+  /**
+   * Person listesini sayfaya render eder
+   */
+  renderPersonList(getFromLocalStorage("personList"));
+  
+  /**
+   * Formun submit olayinda addPerson fonksiyonunu cagirir
+   */
+  formPerson.addEventListener("submit", addPerson);
