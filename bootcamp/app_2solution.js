22020ef1635a9a15a64afdc1c6906d72946a626b
usirin01 <usirin2021@gmail.com>
2022-02-05 11:48:08 +0100

    bootcamp book task was finished

---
+++ bootcamp/app_2solution.js
@@ -0,0 +1,155 @@
+
+
+let = bookInformation [
+    {
+        bookTitle: "Cemile",
+        authors: ["Orhan Kemal"],
+        genre: "Drama",
+        price: 13.70,
+        lendTime:15,
+        amount: 12,
+        lendedPeople: [
+           {
+                firstname: "Mehmet",
+                lastname: "Seker",
+                dateOfLend: "12.01.2022"
+            }
+        ]
+    },
+    {
+        bookTitle: "Müfettisler Mufettisi",
+        authors: [ "Orhan Kemal"],
+        genre: "Drama",
+        price: 15.70,
+        lendTime:15,
+        amount: 1,
+        lendedPeople: []
+    },
+    {
+        bookTitle: "Green Mile",
+        authors: [ "Stefen King"],
+        genre: "Science Fiction",
+        price: 18.70,
+        lendTime:15,
+        amount: 22,
+        lendedPeople: []
+    },
+    {
+        bookTitle: "Illimunati",
+        authors: ["Dan Brown"],
+        genre: "Mystery",
+        price: 18.70,
+        lendTime:15,
+        amount: 3,
+        lendedPeople: []
+    },
+    {
+        bookTitle: "Das Verlorene Symbol",
+        authors: ["Dan Brown"],
+        genre: "Mystery",
+        price: 16.70,
+        lendTime:15,
+        amount: 56,
+        lendedPeople: []
+    },
+    {
+        bookTitle: "Ask",
+        authors: ["Elif Safak"],
+        genre: "Mystery",
+        price: 17.70,
+        lendTime:15,
+        amount: 32,
+        lendedPeople: []
+    },
+    {
+        bookTitle: "Medyum",
+        authors: ["Stephen King"],
+        genre: "Mystery",
+        price: 17.70,
+        lendTime:15,
+        amount: 77,
+        lendedPeople: []
+    }, 
+    {
+        bookTitle: "Sadist",
+        authors: ["Stephen King"],
+        genre: "Mystery",
+        price: 17.70,
+        lendTime:15,
+        amount: 66,
+        lendedPeople: []
+    },
+    {
+        bookTitle: "Institute",
+        authors: ["Stephen King"],
+        genre: "Mystery",
+        price: 17.70,
+        lendTime:15,
+        amount: 88,
+        lendedPeople: []
+    },
+    {
+        bookTitle: "Da Vinci Code",
+        authors: ["Dan Brown"],
+        genre: "Mystery",
+        price: 17.70,
+        lendTime:15,
+        amount: 3,
+        lendedPeople: []
+    },
+    {
+        bookTitle: "Inferno",
+        authors: ["Dan Brown"],
+        genre: "Mystery",
+        price: 17.70,
+        lendTime:15,
+        amount: 19,
+        lendedPeople: []
+    }, 
+    {
+        bookTitle: "Cali Kusu",
+        authors: ["Resat Nuri Güntekin"],
+        genre: "Drama",
+        price: 17.70,
+        lendTime:15,
+        amount: 12,
+        lendedPeople: []
+    },
+    {
+        bookTitle: "The Shining",
+        authors: ["Stephen King"],
+        genre: "Horror",
+        price: 17.70,
+        lendTime:15,
+        amount: 121,
+        lendedPeople: []
+    },
+    {
+        bookTitle: "1984",
+        authors: ["George Orwel"],
+        genre: "Political",
+        price: 19.70,
+        lendTime:15,
+        amount: 43,
+        lendedPeople: []
+    },
+    {
+        bookTitle: "Missing Sister",
+        authors: ["Lucinda Riley"],
+        genre: "Romance",
+        price: 11.70,
+        lendTime:15,
+        amount: 12,
+        lendedPeople: []
+    },
+    {
+        bookTitle: "Our Violent Ends",
+        authors: ["Chloe Gong"],
+        genre: "Romance",
+        price: 11.90,
+        lendTime:15,
+        amount: 56,
+        lendedPeople: []
+    }
+] 
+
---
+++ bootcamp/app_3.js
@@ -0,0 +1,509 @@
+const books = [
+    {
+        bookTitle: "Cemile",
+        authors: [{
+            name: "Orhan",
+            lastname: "Kemal",
+            country: "Turkey"
+        }],
+        genre: "Drama",
+        price: 13.70,
+        lendTime: 15,
+        amount: 12,
+        lendedPeople: [
+            {
+                firstname: "Huseyin",
+                lastname: "Kral Mentor",
+                dateOfLend: "12.10.2021"
+            }
+        ]
+    },
+    {
+        bookTitle: "Chang Ching Chong Hehe",
+        authors: [{
+            name: "Chu",
+            lastname: "Lee",
+            country: "China"
+        }, {
+            name: "Fhu",
+            lastname: "Dragon",
+            country: "China"
+        }],
+        genre: "Drama",
+        price: 15.70,
+        lendTime: 15,
+        amount: 1,
+        lendedPeople: [{
+            firstname: "Huseyin",
+            lastname: "Ciftatli",
+            dateOfLend: "12.01.2022",
+        },
+        {
+            firstname: "Bilal Said",
+            lastname: "Ciledolu",
+            dateOfLend: "11.01.2020",
+        },
+        {
+            firstname: "Umut",
+            lastname: "Gungor",
+            dateOfLend: "15.8.2021",
+        }]
+    },
+    {
+        bookTitle: "Red Cross",
+        authors: [{
+            name: "Chu",
+            lastname: "Lee",
+            country: "China"
+        }, {
+            name: "Fhu",
+            lastname: "Dragon",
+            country: "China"
+        }],
+        genre: "Science Fiction",
+        price: 18.70,
+        lendTime: 15,
+        amount: 22,
+        lendedPeople: [{
+            firstname: "Huseyin",
+            lastname: "Atsiz",
+            dateOfLend: "12.01.2022",
+        },
+        {
+            firstname: "Bilal Said",
+            lastname: "Kaygisiz",
+            dateOfLend: "23.04.2020",
+        },
+        {
+            firstname: "Mehmet",
+            lastname: "Sekeroglu",
+            dateOfLend: "23.04.2020",
+        }]
+    },
+    {
+        bookTitle: "Illimunati",
+        authors: [{
+            name: "Chu",
+            lastname: "Lee",
+            country: "China"
+        }, {
+            name: "Fhu",
+            lastname: "Dragon",
+            country: "China"
+        }],
+        genre: "Mystery",
+        price: 18.70,
+        lendTime: 15,
+        amount: 3,
+        lendedPeople: [{
+            firstname: "Huseyin",
+            lastname: "Atsiz",
+            dateOfLend: "12.01.2022",
+        },
+        {
+            firstname: "Bilal Said",
+            lastname: "Kaygisiz",
+            dateOfLend: "23.04.2020",
+        },
+        {
+            firstname: "Mehmet",
+            lastname: "Sekergibi",
+            dateOfLend: "23.04.2020",
+        }]
+    },
+    {
+        bookTitle: "Das Verlorene Symbol",
+        authors: [{
+            name: "Chu",
+            lastname: "Lee",
+            country: "China"
+        }, {
+            name: "Fhu",
+            lastname: "Dragon",
+            country: "China"
+        }],
+        genre: "Mystery",
+        price: 16.70,
+        lendTime: 15,
+        amount: 56,
+        lendedPeople: [
+            {
+                firstname: "Huseyin",
+                lastname: "Baskan",
+                dateOfLend: "12.01.2022",
+            },
+            {
+                firstname: "Bilal Said",
+                lastname: "Kral Metor",
+                dateOfLend: "23.04.2020",
+            },
+            {
+                firstname: "Umut",
+                lastname: "Gungorler",
+                dateOfLend: "12.01.2022",
+            },
+            {
+                firstname: "Erhan",
+                lastname: "Soyadine",
+                dateOfLend: "23.04.2020",
+            }
+        ]
+    },
+    {
+        bookTitle: "Ask",
+        authors: [{
+            name: "Chu",
+            lastname: "Lee",
+            country: "China"
+        }, {
+            name: "Fhu",
+            lastname: "Dragon",
+            country: "China"
+        }],
+        genre: "Mystery",
+        price: 17.70,
+        lendTime: 15,
+        amount: 32,
+        lendedPeople: [{
+            firstname: "Huseyin",
+            lastname: "Bellissimo",
+            dateOfLend: "12.07.2021",
+        },
+        {
+            firstname: "Bilal Said",
+            lastname: "Kaygisiz",
+            dateOfLend: "23.04.2020",
+        }]
+    },
+    {
+        bookTitle: "Medyum",
+        authors: [{
+            name: "Chu",
+            lastname: "Lee",
+            country: "China"
+        }, {
+            name: "Fhu",
+            lastname: "Dragon",
+            country: "China"
+        }],
+        genre: "Mystery",
+        price: 17.70,
+        lendTime: 15,
+        amount: 77,
+        lendedPeople: []
+    },
+    {
+        bookTitle: "Sadist",
+        authors: [{
+            name: "Chu",
+            lastname: "Lee",
+            country: "China"
+        }, {
+            name: "Fhu",
+            lastname: "Dragon",
+            country: "China"
+        }],
+        genre: "Mystery",
+        price: 17.70,
+        lendTime: 15,
+        amount: 66,
+        lendedPeople: [{
+            firstname: "Huseyin",
+            lastname: "Baskan",
+            dateOfLend: "12.01.2022",
+        },
+        {
+            firstname: "Bilal Said",
+            lastname: "Kral Metor",
+            dateOfLend: "23.04.2020",
+        },
+        {
+            firstname: "Umut",
+            lastname: "Gungorler",
+            dateOfLend: "12.01.2022",
+        },
+        {
+            firstname: "Erhan",
+            lastname: "Soyadine",
+            dateOfLend: "23.04.2020",
+        }]
+    },
+    {
+        bookTitle: "Institute",
+        authors: [{
+            name: "Chu",
+            lastname: "Lee",
+            country: "China"
+        }, {
+            name: "Fhu",
+            lastname: "Dragon",
+            country: "China"
+        }],
+        genre: "Mystery",
+        price: 17.70,
+        lendTime: 15,
+        amount: 88,
+        lendedPeople: [{
+            firstname: "Huseyin",
+            lastname: "Baskan",
+            dateOfLend: "12.01.2022",
+        },
+        {
+            firstname: "Bilal Said",
+            lastname: "Kral Metor",
+            dateOfLend: "23.04.2020",
+        },
+        {
+            firstname: "Umut",
+            lastname: "Gungorler",
+            dateOfLend: "12.01.2022",
+        },
+        {
+            firstname: "Erhan",
+            lastname: "Soyadine",
+            dateOfLend: "23.04.2020",
+        }]
+    },
+    {
+        bookTitle: "Da Vinci Code",
+        authors: [{
+            name: "Chu",
+            lastname: "Lee",
+            country: "China"
+        }, {
+            name: "Fhu",
+            lastname: "Dragon",
+            country: "China"
+        }],
+        genre: "Mystery",
+        price: 17.70,
+        lendTime: 15,
+        amount: 3,
+        lendedPeople: [{
+            firstname: "Huseyin",
+            lastname: "Baskan",
+            dateOfLend: "12.01.2022",
+        },
+        {
+            firstname: "Bilal Said",
+            lastname: "Kral Metor",
+            dateOfLend: "23.04.2020",
+        },
+        {
+            firstname: "Umut",
+            lastname: "Gungorler",
+            dateOfLend: "12.01.2022",
+        },
+        {
+            firstname: "Erhan",
+            lastname: "Soyadine",
+            dateOfLend: "23.04.2020",
+        }]
+    },
+    {
+        bookTitle: "Inferno",
+        authors: [{
+            name: "Chu",
+            lastname: "Lee",
+            country: "China"
+        }, {
+            name: "Fhu",
+            lastname: "Dragon",
+            country: "China"
+        }],
+        genre: "Mystery",
+        price: 17.70,
+        lendTime: 15,
+        amount: 19,
+        lendedPeople: [{
+            firstname: "Huseyin",
+            lastname: "Baskan",
+            dateOfLend: "12.01.2022",
+        },
+        {
+            firstname: "Bilal Said",
+            lastname: "Kral Metor",
+            dateOfLend: "23.04.2020",
+        },
+        {
+            firstname: "Umut",
+            lastname: "Gungorler",
+            dateOfLend: "12.01.2022",
+        },
+        {
+            firstname: "Erhan",
+            lastname: "Soyadine",
+            dateOfLend: "23.04.2020",
+        }]
+    },
+    {
+        bookTitle: "Cali Kusu",
+        authors: [{
+            name: "Chu",
+            lastname: "Lee",
+            country: "China"
+        }, {
+            name: "Fhu",
+            lastname: "Dragon",
+            country: "China"
+        }],
+        genre: "Drama",
+        price: 17.70,
+        lendTime: 15,
+        amount: 12,
+        lendedPeople: [{
+            firstname: "Huseyin",
+            lastname: "Baskan",
+            dateOfLend: "12.01.2022",
+        },
+        {
+            firstname: "Bilal Said",
+            lastname: "Kral Metor",
+            dateOfLend: "23.04.2020",
+        },
+        {
+            firstname: "Umut",
+            lastname: "Gungorler",
+            dateOfLend: "12.01.2022",
+        },
+        {
+            firstname: "Erhan",
+            lastname: "Soyadine",
+            dateOfLend: "23.04.2020",
+        }]
+    },
+    {
+        bookTitle: "The Shining",
+        authors: [{
+            name: "Chu",
+            lastname: "Lee",
+            country: "China"
+        }, {
+            name: "Fhu",
+            lastname: "Dragon",
+            country: "China"
+        }],
+        genre: "Horror",
+        price: 17.70,
+        lendTime: 15,
+        amount: 121,
+        lendedPeople: [{
+            firstname: "Huseyin",
+            lastname: "Baskan",
+            dateOfLend: "12.01.2022",
+        },
+        {
+            firstname: "Bilal Said",
+            lastname: "Kral Metor",
+            dateOfLend: "23.04.2020",
+        },
+        {
+            firstname: "Umut",
+            lastname: "Gungorler",
+            dateOfLend: "12.01.2022",
+        },
+        {
+            firstname: "Erhan",
+            lastname: "Soyadine",
+            dateOfLend: "23.04.2020",
+        }]
+    },
+    {
+        bookTitle: "1984",
+        authors: [{
+            name: "Chu",
+            lastname: "Lee",
+            country: "China"
+        }, {
+            name: "Fhu",
+            lastname: "Dragon",
+            country: "China"
+        }],
+        genre: "Political",
+        price: 19.70,
+        lendTime: 15,
+        amount: 43,
+        lendedPeople: [{
+            firstname: "Huseyin",
+            lastname: "Baskan",
+            dateOfLend: "12.01.2022",
+        },
+        {
+            firstname: "Bilal Said",
+            lastname: "Kral Metor",
+            dateOfLend: "23.04.2020",
+        },
+        {
+            firstname: "Umut",
+            lastname: "Gungorler",
+            dateOfLend: "12.01.2022",
+        },
+        {
+            firstname: "Erhan",
+            lastname: "Soyadine",
+            dateOfLend: "23.04.2020",
+        }]
+    },
+    {
+        bookTitle: "Missing Sister",
+        authors:[{
+            name: "Chu",
+            lastname: "Lee",
+            country: "China"
+        }, {
+            name: "Fhu",
+            lastname: "Dragon",
+            country: "China"
+        }],
+        genre: "Romance",
+        price: 11.70,
+        lendTime: 15,
+        amount: 12,
+        lendedPeople: [{
+            firstname: "Huseyin",
+            lastname: "Baskan",
+            dateOfLend: "12.01.2022",
+        },
+        {
+            firstname: "Bilal Said",
+            lastname: "Kral Metor",
+            dateOfLend: "23.04.2020",
+        },
+        {
+            firstname: "Umut",
+            lastname: "Gungorler",
+            dateOfLend: "12.01.2022",
+        },
+        {
+            firstname: "Erhan",
+            lastname: "Soyadine",
+            dateOfLend: "23.04.2020",
+        }]
+    }
+] 
+
+// let findCheapBook =books
+//     .filter((book) => book.price > 18).length
+//     console.log(findCheapBook)
+
+// function renderBook () {
+//     let bookName = document.querySelector("#booktable");
+// }
+
+const print = (pArray) =>{
+    let bookInformation = document.querySelector("#tablebody");
+    bookInformation.innerHTML = "";
+    pArray.map((pBook, index) => {
+        bookInformation.innerHTML += `
+        <tr>
+                <td>${index}</td>
+                <td>${pBook.bookTitle}</td>
+                <td>${pBook.genre}</td>
+                <td>${pBook.price}</td>
+                <td>${pBook.amount}</td>
+            </tr>
+        `
+    })
+}
+
+print();
